8acebf4df63e8efd08553cf8af3787ac5cd8da02
EnderNinjaBoi <ldkourtev@gmail.com>
2023-06-08 02:05:25 -0400

    small refactor

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,23 @@
 use std::{
-    fs::{self, ReadDir, DirEntry},
+    fs::{self, DirEntry, ReadDir},
     path::{Path, PathBuf},
+    vec,
 };
 
 use clap::Parser;
 use regex::Regex;
 
-#[derive(Parser, Debug)]
+#[derive(Parser, Debug, Clone)]
 #[command(author, version, about, long_about = None)]
 struct Args {
     #[arg(short = 'F', long)]
     fixed_string: bool,
 
-    #[arg(short, long)]
-    recursive: bool,
-
-    #[arg(long)]
+    #[arg(short = 'D', long)]
     debug: bool,
 
     #[arg(short = 'j', long, default_value_t = false)]
     multi_thread: bool,
-
-    #[arg(short = 'd', long, default_value_t = 16)]
-    max_depth: usize,
 
     pattern: String,
     dir: String,
@@ -36,9 +31,7 @@ impl Dir {
     fn from_path(path: &PathBuf) -> Option<Self> {
         let dir_iter = fs::read_dir(path);
         match dir_iter {
-            Ok(iter) => Some(Self {
-                iter,
-            }),
+            Ok(iter) => Some(Self { iter }),
             Err(_) => None,
         }
     }
@@ -46,94 +39,70 @@ impl Dir {
 
 struct DirIter {
     dirs: Vec<Dir>,
-    recurse: bool,
     debug: bool,
-    max_depth: usize,
-    depth: usize,
 }
 
 impl DirIter {
-    fn new(start: PathBuf, recurse: bool, debug: bool, max_depth: usize) -> Self {
+    fn new(start: PathBuf, debug: bool) -> Self {
         let root = Dir::from_path(&start).unwrap();
-        let mut dirs = Vec::with_capacity(max_depth);
-        dirs.push(root);
         Self {
-            dirs,
-            recurse,
+            dirs: vec![root],
             debug,
-            max_depth,
-            depth: 0,
         }
     }
 
-    fn handle_entry(&mut self, entry: DirEntry) -> Option<PathBuf> {
+    fn handle_entry(&mut self, entry: DirEntry) -> Option<DirEntry> {
         let path = entry.path();
         let metadata = fs::metadata(path.as_path());
         match metadata {
-            Ok(metadata) => {
-                let ft = metadata.file_type();
-                if ft.is_symlink() {
-                    self.next()
-                } else if ft.is_dir() {
-                    if !self.recurse {
-                        return self.next();
-                    }
-                    self.step_into_and_get_next(path)
-                } else {
-                    Some(path)
+            Ok(md) => {
+                if md.is_symlink() {
+                    return None;
                 }
-            },
-            Err(e) => {
-                if self.debug { 
-                    eprintln!("Error at path '{}': {:?}", path.display(), e);
+
+                if md.is_dir() {
+                    self.dirs.push(Dir::from_path(&path).unwrap());
+                    return None;
                 }
-                self.next()
+
+                return Some(entry);
+            }
+            Err(er) => {
+                if self.debug {
+                    eprintln!("Error: {:?} | '{}'", er, path.display());
+                }
+                None
             }
         }
     }
 
-    fn step_up_and_get_next(&mut self) -> Option<PathBuf> {
-        self.dirs.pop();
-        if self.depth > 0 {
-            self.depth -= 1;
-        }
-        self.next()
-    }
-
-    fn step_into_and_get_next(&mut self, path: PathBuf) -> Option<PathBuf> {
-        if self.depth + 1 < self.max_depth {
-            let new_dir = Dir::from_path(&path);
-            match new_dir {
-                Some(dir) => {
-                    self.dirs.push(dir);
-                    self.depth += 1;
-                },
-                None => {},
-            };
-        }
-        self.next()
+    fn step_out(&mut self) {
+        self.dirs.pop().expect("BUG: emptry stack (so)");
     }
 }
 
 impl Iterator for DirIter {
-    type Item = PathBuf;
+    type Item = DirEntry;
 
     fn next(&mut self) -> Option<Self::Item> {
-        if self.dirs.len() == 0 {
-            return None;
-        }
-        let entry = self.dirs[self.depth].iter.next();
-        match entry {
-            Some(val) => {
-                match val {
-                    Ok(path) => self.handle_entry(path),
-                    Err(_) => self.next(),
+        while !self.dirs.is_empty() {
+            let entry = self
+                .dirs
+                .last_mut()
+                .expect("BUG: empty stack (gd)")
+                .iter
+                .next();
+            match entry {
+                Some(Ok(path)) => {
+                    if let Some(val) = self.handle_entry(path) {
+                        return Some(val);
+                    }
                 }
-            },
-            None => {
-                self.step_up_and_get_next()
-            }
+                Some(Err(_)) => return None,
+                None => self.step_out(),
+            };
         }
+        None
     }
 }
 
@@ -147,11 +116,12 @@ fn single_iter(args: Args) {
     // no need to check for directory here, will be handled by iterator
     let path = Path::new(&args.dir);
 
-    let iter = DirIter::new(path.to_path_buf(), args.recursive, args.debug, args.max_depth);
+    let iter = DirIter::new(path.to_path_buf(), args.debug);
     iter.for_each(|x| {
-        let string = x.to_str().unwrap();
-        if regex.is_match(string) {
-            println!("{}", string);
+        let dir = x;
+        let path = dir.path();
+        if regex.is_match(path.to_str().unwrap()) {
+            println!("{}", path.display());
         }
     });
 }
@@ -159,5 +129,5 @@ fn single_iter(args: Args) {
 fn main() {
     let args = Args::parse();
 
-    single_iter(args);
+    single_iter(args.clone());
 }
